a555d3862a5ddf1e9c979798582aad100312bc4b
vananh <vananhbui41@gmail.com>
2022-05-08 18:30:41 +0700

    Model Relationships

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Member extends Model
 {
     use HasFactory;
+    public function events() {
+        return $this->belongsToMany(Event::class);
+    }
+
+    public function coupons() {
+        return $this->belongsToMany(Coupon::class);
+    }
 }
